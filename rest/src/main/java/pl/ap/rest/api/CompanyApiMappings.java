4b2d4fb4837e93303ffd08b98c723912832f58e3
Paweł Radomski <pawel.radomski@efl.com.pl>
2014-09-17 23:08:10 +0200

    working rest init

--- rest/src/main/java/pl/ap/rest/api/CompanyApiMappings.java
+++ rest/src/main/java/pl/ap/rest/api/CompanyApiMappings.java
@@ -6,17 +6,20 @@ package pl.ap.rest.api;
 public interface CompanyApiMappings
 {
    public static final String REST_PREFIX = "";
-//   public static final String REST_PREFIX = "/rest";
+
+//   APP
+
+   public static final String GET_CURRENT_USER = REST_PREFIX + "/user/current";
+
+   public static final String GET_MENU = REST_PREFIX + "/menu";
+
+//   NEWS
 
    public static final String GET_NEWS_LIST = REST_PREFIX + "/news/list";
 
    public static final String CREATE_NEWS = REST_PREFIX + "/news";
 
    public static final String GET_NEWS = REST_PREFIX + "/news/{" + ApiKeys.SID + "}";
-
-   public static final String GET_CURRENT_USER = REST_PREFIX + "/user/current";
-
-   public static final String GET_MENU = REST_PREFIX + "/menu";
 
 //   ROOMS
 
