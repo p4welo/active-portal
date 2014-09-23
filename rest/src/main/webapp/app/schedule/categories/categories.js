b054b858ce79fe8bea87e6fcae902746caaa42c6
Paweł Radomski <pawel.radomski@efl.com.pl>
2014-09-23 13:16:26 +0200

    categories

--- rest/src/main/webapp/app/schedule/categories/categories.js
+++ rest/src/main/webapp/app/schedule/categories/categories.js
@@ -1,8 +1,24 @@
 define([
-    'schedule/module'
+    'schedule/module',
+    'schedule/categories/modal/addCategory',
+    'services/notificationService',
+    'services/categoryService'
 ], function (module) {
 
-    module.controller("categoriesController", function ($scope) {
+    module.controller("categoriesController", function ($scope, $modal, categoryFactory, notificationService) {
+        $scope.categories = categoryFactory.find();
 
+        $scope.add = function() {
+            var modalInstance = $modal.open(
+                {
+                    templateUrl: 'app/schedule/categories/modal/addCategory.html',
+                    controller: "addCategoryController"
+                });
+
+            modalInstance.result.then(function () {
+                $scope.categories = categoryFactory.find();
+                notificationService.success("Pomyślnie zapisano");
+            });
+        };
     });
 });
---
+++ rest/src/main/webapp/app/schedule/categories/modal/addCategory.js
@@ -0,0 +1,19 @@
+define([
+    'schedule/module',
+    'services/categoryService'
+], function (module) {
+
+    module.controller('addCategoryController', function ($scope, $modalInstance, categoryFactory) {
+        $scope.category = {};
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+
+        $scope.save = function (category) {
+            categoryFactory.create(category);
+            $modalInstance.close();
+        }
+    });
+
+});
