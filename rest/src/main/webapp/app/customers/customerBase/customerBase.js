c28d4239b30a92afece5954d93e511bbea1e4775
p4welo <p4welo@gmail.com>
2014-09-24 10:22:20 +0200

    Customer Base

--- rest/src/main/webapp/app/customers/customerBase/customerBase.js
+++ rest/src/main/webapp/app/customers/customerBase/customerBase.js
@@ -3,6 +3,25 @@ define([
 ], function (module) {
 
     module.controller("customerBaseController", function ($scope) {
+
+        $scope.customers = [
+            {"firstName": "Anna", "lastName": "Fotyga", "gender": "F", "mobile": "789132665"},
+            {"firstName": "Andrzej", "lastName": "Piaseczny", "gender": "M", "mobile": "556025123"},
+            {"firstName": "Łukasz", "lastName": "Zagrobelny", "gender": "M", "mobile": "745212335"},
+            {"firstName": "Monika", "lastName": "Kudyba", "gender": "F", "mobile": "995210236"},
+            {"firstName": "Władysław", "lastName": "Pasikowski", "gender": "M", "mobile": "770550236"},
+            {"firstName": "Szczepan", "lastName": "Oleksy", "gender": "M", "mobile": "112556323"},
+            {"firstName": "Dagmara", "lastName": "Królikowska", "gender": "F", "mobile": "505123220"},
+            {"firstName": "Honorata", "lastName": "Siudym", "gender": "F", "mobile": "793096623"},
+            {"firstName": "Jacek", "lastName": "Gzella", "gender": "M", "mobile": "500120002"},
+            {"firstName": "Zbigniew", "lastName": "Zamojski", "gender": "M", "mobile": "723556852"},
+            {"firstName": "Anna", "lastName": "Przybylska", "gender": "F", "mobile": "601202885"},
+            {"firstName": "Martyna", "lastName": "Olejnik", "gender": "F", "mobile": "506995888"},
+            {"firstName": "Barbara", "lastName": "Krotoszyńska", "gender": "F", "mobile": "603338879"},
+            {"firstName": "Paweł", "lastName": "Kukiz", "gender": "M", "mobile": "793432025"},
+            {"firstName": "Adam", "lastName": "Bednorz", "gender": "M", "mobile": "792589660"}
+        ];
+
         $scope.styles = [
             {"sid": "43223mcnv343445289vjkb0011168103", "id": 6, "name": "Hip hop", "category": {"sid": "43632299f3434e0889c96b50c1168103", "id": 1, "name": "Zajęcia solo", "code": "solo"}},
             {"sid": "43223mcnv343445289vjkb0585168103", "id": 7, "name": "Reggaeton", "category": {"sid": "43632299f3434e0889c96b50c1168103", "id": 1, "name": "Zajęcia solo", "code": "solo"}},
