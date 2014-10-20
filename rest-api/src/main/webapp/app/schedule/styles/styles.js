489d8a2c112bd1abf986b6f67ccf71232f24362e
p4welo <p4welo@gmail.com>
2014-10-20 10:52:10 +0200

    objectState translation FIX

--- rest-api/src/main/webapp/app/schedule/styles/styles.js
+++ rest-api/src/main/webapp/app/schedule/styles/styles.js
@@ -3,20 +3,24 @@ define([
     'schedule/styles/modal/addStyle',
     'services/styleService',
     'services/categoryService',
-    'services/notificationService'
-], function (module) {
+    'services/notificationService',
+    'utils'
+], function (module, utils) {
 
     module.controller("stylesController", function ($scope, $modal, styleFactory, styleService, categoryFactory, notificationService) {
+
+        var EDIT_STYLE_KEY = 'edit';
+        var EDIT_CATEGORY_KEY = 'editcategory';
+
 
         $scope.styles = styleFactory.findAll();
         $scope.categories = categoryFactory.findAll();
 
         $scope.add = function () {
-            var modalInstance = $modal.open(
-                {
-                    templateUrl: 'app/schedule/styles/modal/addStyle.html',
-                    controller: "addStyleController"
-                });
+            var modalInstance = $modal.open({
+                templateUrl: 'app/schedule/styles/modal/addStyle.html',
+                controller: "addStyleController"
+            });
 
             modalInstance.result.then(function () {
                 $scope.styles = styleFactory.findAll();
@@ -30,27 +34,28 @@ define([
                 return;
             }
             $scope.selected = styleService.copyProperties(style);
-            $scope.selected.edit = false;
-            $scope.selected.editcategory = false;
+            $scope.selected[EDIT_STYLE_KEY] = false;
+            $scope.selected[EDIT_CATEGORY_KEY] = false;
         }
 
         $scope.editCategory = function (style) {
-            style.editcategory = true;
+            style[EDIT_CATEGORY_KEY] = true;
             $scope.category = style.category;
         }
 
         $scope.setCategory = function (style) {
             styleFactory.setCategory({sid: style.sid}, style.category).$promise.then(
                 function () {
-                    style.editcategory = false;
+                    style[EDIT_CATEGORY_KEY] = false;
                     $scope.styles = styleFactory.findAll();
                     notificationService.success("Pomyślnie zapisano");
                 });
         }
 
         $scope.update = function (style) {
-            delete style['edit'];
-            delete style['editcategory'];
+            delete style[EDIT_STYLE_KEY];
+            delete style[EDIT_CATEGORY_KEY];
+
             styleFactory.update({ sid: style.sid }, style).$promise.then(
                 function () {
                     notificationService.success("Pomyślnie zapisano");
@@ -58,8 +63,8 @@ define([
                 });
         }
 
-        $scope.resolveStatusCss = function (style) {
-            return {'label-success': style.objectState == 'ACTIVE', 'label-danger': style.objectState == 'INACTIVE'}
+        $scope.resolveObjectStateCss = function (obj) {
+            return {'label-success': obj.objectState == 'ACTIVE', 'label-danger': obj.objectState == 'INACTIVE'}
         }
     });
 });
