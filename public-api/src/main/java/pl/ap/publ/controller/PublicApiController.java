cd90684d60baca54070edb7340cb5316304ba98f
p4welo <p4welo@gmail.com>
2014-10-24 16:27:51 +0200

    cors 7

--- public-api/src/main/java/pl/ap/publ/controller/PublicApiController.java
+++ public-api/src/main/java/pl/ap/publ/controller/PublicApiController.java
@@ -76,10 +76,6 @@ public class PublicApiController {
         LOGGER.info("sendFeedback()");
 
         Assert.notNull(feedback);
-        response.setHeader("Access-Control-Allow-Origin", "*");
-        response.setHeader("Access-Control-Allow-Methods", "POST, GET, OPTIONS, DELETE");
-        response.setHeader("Access-Control-Max-Age", "3600");
-        response.setHeader("Access-Control-Allow-Headers", "x-requested-with");
 
         emailService.customerFeedback(feedback);
     }
