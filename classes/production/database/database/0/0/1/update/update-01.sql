9bcfe9e0e38c6b76f97b5aed5f566f8dd84b920e
p4welo <p4welo@gmail.com>
2015-01-14 16:23:10 +0100

    jakies stare pliki db

--- classes/production/database/database/0/0/1/update/update-01.sql
+++
@@ -1,8 +0,0 @@
-BEGIN;
-
-ALTER TABLE dance_class DROP COLUMN start_time;
-ALTER TABLE dance_class DROP COLUMN end_time;
-ALTER TABLE dance_class ADD COLUMN start_time TIME NOT NULL;
-ALTER TABLE dance_class ADD COLUMN end_time TIME NOT NULL;
-
-COMMIT;
