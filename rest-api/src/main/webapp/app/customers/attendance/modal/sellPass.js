002898e2676162fd4f41d543ed693557d766009a
p4welo <p4welo@gmail.com>
2015-01-15 18:03:27 +0100

    sell pass

--- rest-api/src/main/webapp/app/customers/attendance/modal/sellPass.js
+++ rest-api/src/main/webapp/app/customers/attendance/modal/sellPass.js
@@ -7,13 +7,40 @@ define([
         $scope.cancel = function () {
             $modalInstance.dismiss();
         }
+        $scope.page = 0;
 
+//        CUSTOMER
+        $scope.newCustomer = {};
         $scope.customers = customerHttpClient.findAll();
-        $scope.customerSubview = 0;
-        $scope.setSubview = function (viewId) {
-            $scope.customerSubview = viewId;
+        $scope.existingCustomer = true;
+        $scope.setExistingCustomer = function (isExistingCustomer) {
+            $scope.existingCustomer = isExistingCustomer;
+        }
+        $scope.selectCustomer = function (customer) {
+            if ($scope.customer == customer) {
+                $scope.customer = null;
+                return;
+            }
+            $scope.customer = customer;
+        }
+        $scope.isValidCustomer = function () {
+            var validNew = !$scope.existingCustomer
+                && $scope.newCustomer.firstName != null
+                && $scope.newCustomer.lastName != null
+                && $scope.newCustomer.gender != null
+                && $scope.newCustomer.mobile != null;
+
+            var validExisting = $scope.existingCustomer && $scope.customer != null;
+            return validNew || validExisting;
+        }
+        $scope.passPage = function () {
+            $scope.page = 1;
         }
 
+//        PASS
+        $scope.customerPage = function () {
+            $scope.page = 0;
+        }
     });
 
 });
