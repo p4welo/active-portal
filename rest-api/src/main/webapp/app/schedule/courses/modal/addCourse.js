27da26d7ab60620cbc60e393187b547d3042c1d4
p4welo <p4welo@gmail.com>
2015-02-01 00:05:16 +0100

    combo boxes

--- rest-api/src/main/webapp/app/schedule/courses/modal/addCourse.js
+++ rest-api/src/main/webapp/app/schedule/courses/modal/addCourse.js
@@ -8,6 +8,14 @@ define([
 
     module.controller('addCourseController', function ($scope, $modalInstance, courseHttpClient, styleHttpClient, instructorHttpClient, roomHttpClient) {
         $scope.course = {};
+        $scope.start = {
+            hours: "15",
+            minutes: "00"
+        }
+        $scope.end = {
+            hours: "16",
+            minutes: "00"
+        }
 
         $scope.styles = styleHttpClient.findAll();
         $scope.instructors = instructorHttpClient.findAll();
@@ -21,12 +29,24 @@ define([
         $scope.types = [
             "TYPE_REGISTRATION", "TYPE_OPEN", "TYPE_CLOSED"
         ];
+        $scope.hours = [];
+        $scope.minutes = [];
+        for (var i = 7; i < 24; i++) {$scope.hours.push("" + i)}
+        for (var i = 0; i < 60; i+=5) {
+            var j = i;
+            if (j < 10) {
+                j = "0" + j;
+            }
+            $scope.minutes.push("" + j);
+        }
 
         $scope.cancel = function () {
             $modalInstance.dismiss('cancel');
         };
 
         $scope.save = function (course) {
+            course.startTime = $scope.start.hours + ":" + $scope.start.minutes;
+            course.endTime = $scope.end.hours + ":" + $scope.end.minutes;
             if (course.type == 'TYPE_REGISTRATION') {
                 course.canRegister = true;
                 course.canJoin = false;
