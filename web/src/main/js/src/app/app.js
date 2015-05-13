01f048d68b7d5aae4fb2b39f6479ef465c1add21
p4welo <p4welo@gmail.com>
2015-05-13 11:18:56 +0200

    courseState

--- web/src/main/js/src/app/app.js
+++ web/src/main/js/src/app/app.js
@@ -11,8 +11,7 @@ angular.module('activePortal', [
     'ngAnimate',
     'ngTouch',
     'pascalprecht.translate',
-    'ui.router'
-    ,
+    'ui.router',
     'angular-loading-bar'
 ])
     .factory("httpAuthInterceptor", function ($q, $window) {
@@ -54,59 +53,6 @@ angular.module('activePortal', [
 
         $urlRouterProvider.otherwise('/dashboard');
 
-        $translateProvider.translations('pl', {
-
-            PN: 'Poniedziałek',
-            WT: 'Wtorek',
-            SR: 'Środa',
-            CZ: 'Czwartek',
-            PT: 'Piątek',
-            SB: 'Sobota',
-            ND: 'Niedziela',
-
-            BEGINNER: "Początkujący",
-            INTERMEDIATE: "Średniozaawansowany",
-            ADVANCED: "Zaawansowany",
-            OPEN: "Otwarty",
-
-            m: "Mała",
-            d: "Duża",
-
-            TYPE_REGISTRATION: "Grupa na zapisy",
-            TYPE_OPEN: "Można dołączyć",
-            TYPE_CLOSED: "Nie można dołączyć",
-
-            INDIVIDUAL_LESSON: "Lekcja indywidualna",
-            WORKSHOPS: "Warsztaty",
-            EXTERNAL: "Wynajem sali",
-
-            ACTIVE: "Aktywny",
-            INACTIVE: "Nieaktywny",
-            DELETED: "Usunięty",
-
-            MALE: "Mężczyzna",
-            FEMALE: "Kobieta",
-
-            AUTH_SYSTEM_USERS: "SYSTEM: Użytkownicy",
-            AUTH_SYSTEM_AUTHORITIES: "SYSTEM: Uprawnienia",
-            AUTH_NEWS_NEWS_LIST: "AKTUALNOŚCI: Lista",
-            AUTH_NEWS_NEWSLETTER: "AKTUALNOŚCI: Newsletter",
-            AUTH_SCHEDULE_ROOMS: "GRAFIK: Pomieszczenia",
-            AUTH_SCHEDULE_INSTRUCTORS: "GRAFIK: Instruktorzy",
-            AUTH_SCHEDULE_CATEGORIES: "GRAFIK: Kategorie zajęć",
-            AUTH_SCHEDULE_STYLES: "GRAFIK: Style taneczne",
-            AUTH_SCHEDULE_SCHEDULE: "GRAFIK: Grafik zajęć",
-            AUTH_CUSTOMERS_CUSTOMER_BASE: "SEKRETARIAT: Baza klientów",
-            AUTH_CUSTOMERS_CUSTOMER_PRESENCE: "SEKRETARIAT: Obecności",
-
-            PASS_ADULT: "DOROŚLI",
-            PASS_CHILD: "DZIECI",
-
-            PERIOD_MONTH: "Miesięcy",
-            PERIOD_DAY: "Dni"
-        });
-        $translateProvider.preferredLanguage('pl');
-
         $httpProvider.interceptors.push("httpAuthInterceptor");
         $httpProvider.interceptors.push("httpErrorInterceptor");
 
@@ -117,82 +63,5 @@ angular.module('activePortal', [
         //$httpProvider.defaults.headers.get['If-Modified-Since'] = 'Mon, 26 Jul 1997 05:00:00 GMT';
         //$httpProvider.defaults.headers.get['Cache-Control'] = 'no-cache';
         //$httpProvider.defaults.headers.get.Pragma = 'no-cache';
-    })
-
-    .run(function ($rootScope, $location, $modal, $log) {
-        $rootScope.isActive = function (obj) {
-            return obj.objectState == 'ACTIVE';
-        };
-        $rootScope.isEqual = function (obj1, obj2) {
-            return obj1 !== undefined && obj2.sid == obj1.sid;
-        };
-        $rootScope.isLocal = function () {
-            return $location.host() == 'localhost';
-        };
-        $rootScope.scan = function () {
-            $modal.open({
-                templateUrl: 'common/modal/scanTicket.tpl.html',
-                controller: "scanTicketDialogCtrl",
-                size: 'sm'
-            });
-        };
-        $rootScope.log = function (message) {
-            $log.debug(message);
-        }
-    })
-
-    .controller("menuCtrl", function ($scope, authorityHttpClient) {
-        $scope.currentAuth = [];
-        authorityHttpClient.getCurrentAuthorities().$promise.then(
-            function (result) {
-                $scope.currentAuth = result;
-            }
-        );
-
-        function hasAuth(key) {
-            if ($scope.currentAuth !== undefined) {
-                return $scope.currentAuth.indexOf(key) > -1;
-            }
-        }
-
-//              SYSTEM
-        $scope.systemAuth = function () {
-            return $scope.usersAuth() || $scope.authoritiesAuth();
-        };
-        $scope.usersAuth = function () {
-            return hasAuth("AUTH_SYSTEM_USERS");
-        };
-        $scope.authoritiesAuth = function () {
-            return hasAuth("AUTH_SYSTEM_AUTHORITIES");
-        };
-//              GRAFIK
-        $scope.scheduleAuth = function () {
-            return $scope.roomsAuth() || $scope.instructorsAuth() || $scope.categoriesAuth() || $scope.stylesAuth() || $scope.coursesAuth();
-        };
-        $scope.roomsAuth = function () {
-            return hasAuth("AUTH_SCHEDULE_ROOMS");
-        };
-        $scope.instructorsAuth = function () {
-            return hasAuth("AUTH_SCHEDULE_INSTRUCTORS");
-        };
-        $scope.categoriesAuth = function () {
-            return hasAuth("AUTH_SCHEDULE_CATEGORIES");
-        };
-        $scope.stylesAuth = function () {
-            return hasAuth("AUTH_SCHEDULE_STYLES");
-        };
-        $scope.coursesAuth = function () {
-            return hasAuth("AUTH_SCHEDULE_SCHEDULE");
-        };
-//              SEKRETARIAT
-        $scope.officeAuth = function () {
-            return $scope.customerBaseAuth() || $scope.attendanceAuth();
-        };
-        $scope.customerBaseAuth = function () {
-            return hasAuth("AUTH_CUSTOMERS_CUSTOMER_BASE");
-        };
-        $scope.attendanceAuth = function () {
-            return hasAuth("AUTH_CUSTOMERS_CUSTOMER_PRESENCE");
-        };
     })
 ;
---
+++ web/src/main/js/src/app/global.js
@@ -0,0 +1,22 @@
+angular.module('activePortal')
+    .run(function ($rootScope, $location, $modal, $log) {
+        $rootScope.isActive = function (obj) {
+            return obj.objectState == 'ACTIVE';
+        };
+        $rootScope.isEqual = function (obj1, obj2) {
+            return obj1 !== undefined && obj2.sid == obj1.sid;
+        };
+        $rootScope.isLocal = function () {
+            return $location.host() == 'localhost';
+        };
+        $rootScope.scan = function () {
+            $modal.open({
+                templateUrl: 'common/modal/scanTicket.tpl.html',
+                controller: "scanTicketDialogCtrl",
+                size: 'sm'
+            });
+        };
+        $rootScope.log = function (message) {
+            $log.debug(message);
+        }
+    });
